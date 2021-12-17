650bfb9378cb722719ed219b7a506315f0a5f1bb
mtanvir9666 <82137693+mtanvir9666@users.noreply.github.com>
2021-12-16 19:17:03 -0800

    First commit

---
+++ src/test/java/com/scalable/uniqueid/UniqueIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalable.uniqueid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
